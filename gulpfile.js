fce59450ce85bf919eeb0f170e4d5c8985dd244b
Manny <kurinaah@hartford.edu>
2025-03-18 20:39:20 -0400

    made final changes and to cleanup code

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,94 @@
-const { series } = require(`gulp`);
+"use strict";
 
-// Import tasks from the build-tools folder
-const compileCSS = require(`./build-tools/compile-css`).compileCSS;
-const compressHTML = require(`./build-tools/compress-html`).compressHTML;
-const compressImages = require(`./build-tools/compress-images`).compressImages;
-const validateHTML = require(`./build-tools/validate-html`).validateHTML;
-const lintJS = require(`./build-tools/lint-js`).lintJS;
+const { src, dest, series, parallel, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const htmlmin = require("gulp-htmlmin");
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+//const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const stylelint = require("gulp-stylelint");
+const eslint = require("gulp-eslint");
+const babel = require("gulp-babel");
+const browserSyncLib = require("browser-sync");
 
-//Defining the build task
-let build = series(compileCSS, compressHTML, compressImages, validateHTML, lintJS);
+// Destructuring the gulp methods
+const browserSync = browserSyncLib.create();
 
-// Define Gulp tasks
-exports.build = build;
-exports.default = build;
+
+// linting the CSS using Stylelint
+function lintCSS() {
+    return src("styles/**/*.css").pipe(
+    stylelint({
+        reporters: [{ formatter: "string", console: true }],
+    })
+    );
+}
+
+
+// Compile CSS
+function compileCSS() {
+    return src("styles/main.css")
+    .pipe(sass({ outputStyle: "expanded", precision: 10 }).on("error", sass.logError))
+    .pipe(dest("dist/css"))
+    .pipe(browserSync.stream());
+}
+
+// Lint JS (Eslint)
+function lintJS() {
+    return src("js/**/*.js")
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+// Transpiling and minifying JS (Babel, uglify)
+function compressJS() {
+    return src("js/**/*.js") // Select all JS files in js folder
+        .pipe(babel({ presets: ["@babel/preset-env"] })) // Transpile to ES5
+        .pipe(uglify()) // Minify JS
+        .pipe(dest("dist/js"));
+    }
+
+// Image Compressor
+function compressImages() {
+    return src("img/**/*.{png,jpg,jpeg,svg,gif}")
+    .pipe(cache(imagemin()))
+    .pipe(dest("dist/img"));
+}
+
+// HTML Compressor
+function compressHTML() {
+    return src("*.html").pipe(htmlmin({ collapseWhitespace: true })).pipe(dest("dist"));
+}
+
+// Watch files and reload browser automatically
+function watchFiles() {
+    watch("styles/**/*.scss", series(lintCSS, compileCSS));
+    watch("*.html", compressHTML);
+    watch("img/**/*", compressImages);
+    watch("js/**/*.js", series(lintJS, compressJS));
+}
+
+//Serve and auto refresh the browser
+function serve() {
+    browserSync.init({
+    server: {
+        baseDir: "./",
+    },
+    });
+    watchFiles();
+    watch("*.html").on("change", browserSync.reload);
+}
+
+
+// Default Task: Runs everything in develpment mode
+
+exports.build = series(
+    parallel(lintCSS, lintJS),
+    parallel(compileCSS, compressJS, compressHTML, compressImages),
+    serve
+);
+
+exports.compressJS = compressJS;
+exports.default = exports.build;
--- js/main.js
+++ js/main.js
@@ -13,24 +13,24 @@
 
 // Dynamically loading data.json
 // Was wrapped as JSONP
-let jsonpScript = document.createElement("script");
-jsonpScript.src = "json/data.json";
+let jsonpScript = document.createElement(`script`);
+jsonpScript.src = `json/data.json`;
 document.body.appendChild(jsonpScript);
 
 // Global callback function for JSONP
 window.albumData = (albumsData) => {
-  // Storing albums array and current index
+    // Storing albums array and current index
     let albums = albumsData;
     let currentIndex = 0;
 
-  // Selecting the container for the slides from index.html file
-    const slidesContainer = document.querySelector(".carousel-slides");
+    // Selecting the container for the slides from index.html file
+    const slidesContainer = document.querySelector(`.carousel-slides`);
 
-  // Function to render the slides based on the given index
+    // Function to render the slides based on the given index
     const renderSlide = (index) => {
-    let album = albums[index];
-    // Building the HTML for a single slide using backticks for string interpolation
-    let slideHTML = `
+        let album = albums[index];
+        // Building the HTML for a single slide using backticks for string interpolation
+        let slideHTML = `
         <div class="slide">
             <h2>${album.artist}</h2>
             <p><strong>${album.album}</strong></p>
@@ -39,30 +39,30 @@ window.albumData = (albumsData) => {
             <a href="${album.review.url}" target="_blank">Read More</a>
         </div>
     `;
-    slidesContainer.innerHTML = slideHTML;
-};
+        slidesContainer.innerHTML = slideHTML;
+    };
 
-  // Initial render: show the first album
+    // Initial render: show the first album
     renderSlide(currentIndex);
 
-  // Adding event listeners for the navigation arrows inside .carousel-navigation
-    const navLinks = document.querySelectorAll(".carousel-navigation a");
-  // Assume the first <a> is the left arrow, second is the right arrow
+    // Adding event listeners for the navigation arrows inside .carousel-navigation
+    const navLinks = document.querySelectorAll(`.carousel-navigation a`);
+    // Assume the first <a> is the left arrow, second is the right arrow
     const prevLink = navLinks[0];
     const nextLink = navLinks[1];
 
-  // On clicking the previous arrow, decrease the index (wrap around if needed)
-    prevLink.addEventListener("click", (e) => {
-    e.preventDefault(); // Prevent default link behavior
-    currentIndex = (currentIndex - 1 + albums.length) % albums.length;
-    renderSlide(currentIndex);
-});
+    // On clicking the previous arrow, decrease the index (wrap around if needed)
+    prevLink.addEventListener(`click`, (e) => {
+        e.preventDefault(); // Prevent default link behavior
+        currentIndex = (currentIndex - 1 + albums.length) % albums.length;
+        renderSlide(currentIndex);
+    });
 
-  // On clicking the next arrow, increase the index (wrap around if needed)
-    nextLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    currentIndex = (currentIndex + 1) % albums.length;
-    renderSlide(currentIndex);
+    // On clicking the next arrow, increase the index (wrap around if needed)
+    nextLink.addEventListener(`click`, (e) => {
+        e.preventDefault();
+        currentIndex = (currentIndex + 1) % albums.length;
+        renderSlide(currentIndex);
     });
 };
 
